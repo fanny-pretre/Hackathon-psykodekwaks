b84291a18e053e3b3437c7f60c0f3ed949a9c2b8
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-07-04 11:56:04 +0200

    Mise en place des cards

--- client/src/pages/Activity/Activity.jsx
+++ client/src/pages/Activity/Activity.jsx
@@ -6,17 +6,32 @@ function Activity() {
   const activities = useLoaderData();
 
   return (
-    <div>
-      <h1> Bonjour je suis Activity</h1>;
-      {activities.map((activity) => (
-        <article key={activity.id}>
-          <h2>{activity.name}</h2>
-          <span>
-            {activity.date} {activity.time}
-          </span>
-          <p>{activity.description}</p>
-        </article>
-      ))}
+    <div className="global-div-activity">
+      <h1>Les activités disponibles</h1>;
+      <section className="all-activities-cards">
+        {activities.map((activity) => (
+          <article className="activity-card" key={activity.id}>
+            <img
+              className="image-activity-card"
+              src="https://img.freepik.com/photos-gratuite/jetee-au-bord-lac-hallstatt-autriche_181624-44201.jpg"
+              alt="paysage"
+            />
+            <div className="activity-info">
+              <h2>{activity.name}</h2>
+              <span>
+                {activity.date} {activity.time}
+              </span>
+              <p>{activity.place}</p>
+              <p className="activity-description">{activity.description}</p>
+            </div>
+            <div className="global-button-activity">
+              <button className="button-participate" type="submit">
+                PARTICIPER
+              </button>
+            </div>
+          </article>
+        ))}
+      </section>
     </div>
   );
 }
